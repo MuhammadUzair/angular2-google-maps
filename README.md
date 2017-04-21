2feb1cf07a037534ff7279047fbc1db678864f89
Muhammad Uzair Khan <muhammaduzair066@hotmail.com>
2017-04-22 03:23:48 +0500

    mapAPP

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Todo
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import {MarkerService} from './services/marker.service';
 
 import { AppComponent } from './app.component';
 
@@ -9,12 +15,17 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
-    HttpModule
-  ],
-  providers: [],
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA1xQH5jsVxy9j_G4bWkKK9vUjV1O6AxUQ'
+    })
+    ],
+  providers: [MarkerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
